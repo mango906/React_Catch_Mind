85897f9175782c7f3db17421bea3952d633ff6cd
mango906 <minkb01@dgsw.hs.kr>
2019-01-23 00:15:05 +0900

    이동해서 방 참가할수있도록 수정

--- src/waiting.js
+++ src/waiting.js
@@ -6,23 +6,52 @@ class waiting extends Component{
 
   state = {
     my_id : null,
-    roomInfo : null
+    roomInfo : [],
+    members : []
   }
+
+  // findName(id){
+  //   let name;
+  //   this.state.members.forEach((member) =>{
+  //     if(id === member.id){
+  //       name = member.name;
+  //     }
+  //     return name;
+  //   })
+  // }
 
   componentDidMount(){
     let url = window.location.href;
     let room_id = url.substring( url.indexOf('=')+1, url.length );
-    socket.emit('getRoomInfo', room_id)
+    socket.emit('getRoomInfo', room_id);
 
-    socket.on('getRoomInfo', (roomInfo) =>{
-      console.log(roomInfo);
-    })
+    socket.on('getRoomInfo', (room_info, clients) =>{
+      this.setState({
+        members : []
+      });
+      clients.forEach(client => {
+        
+        Object.keys(Object.values(room_info)[0]).forEach(info =>{
+          if(client.id === info){
+            this.setState({
+              members : [...this.state.members, client.name]
+            })
+          }
+        })
+      });
+    });
+
   }
 
   render(){
-    // const roomMember = 
+    // console.log(Object.values(this.state.roomInfo)[0]);
+    const members = this.state.members.map((member, i) =>(
+      <li key={i}>{Object.values(member)}</li>
+    ))
     return(
-      <div>안녕</div>
+      <div>
+        {members}
+      </div>
     )
   }
 }
